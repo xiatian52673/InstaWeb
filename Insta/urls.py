9bb1ae48e49ff38dabb5b835d57a4f70fba31b45
xiatian52673 <xiatian52673@gmail.com>
2019-12-17 11:57:45 -0500

    enable CRUD

--- Insta/urls.py
+++ Insta/urls.py
@@ -15,8 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
-from Insta.views import HelloWorld
+
+from Insta.views import HelloWorld, PostView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
 
 urlpatterns = [
-    path('', HelloWorld.as_view(), name='HelloWorld'),
+    path('', HelloWorld.as_view(), name='helloworld'),
+    path('posts/', PostView.as_view(), name = 'posts'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name = 'post_detail'),
+    path('post/new', PostCreateView.as_view(), name = 'make_post'),
+    path('post/update/<int:pk>/', PostUpdateView.as_view(), name = 'post_update'),
+    path('post/delete/<int:pk>/', PostDeleteView.as_view(), name = 'post_delete'),
 ]
